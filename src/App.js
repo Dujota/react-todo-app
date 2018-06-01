cb66608b582187598f523282767a22e1bf064ada
Denis <ddujota@internationalsafety.com>
2018-06-01 11:54:20 -0400

    react the old way started

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const todos = [
+  'Learn React',
+  'Learn Redux',
+  'Learn React-Native'
+]
+
+
+class App extends Component {
+  render() {
+    return React.createElement(
+      'div',
+      {
+        className: 'app'
+      },
+      React.createElement(
+        'div',
+        {
+          className: 'todo-container',
+        },
+        // todos.map(todo =>todo)
+        todos.map((todo, index) => {
+          return React.createElement(
+            'div',
+          {className: "todo-item-row",
+          key: index
+        },
+          todo)
+        })
+      )
+    )
+  }
+}
+
+export default App;
+/*
+  <div class="app">
+      <div class="todo-container">
+        <div class="todo-item-row">Learn React!</div>
+        <div class="todo-item-row">Learn Redux</div>
+        <div class="todo-item-row">Learn React Native</div>
+      </div>
+    </div>
+*/
